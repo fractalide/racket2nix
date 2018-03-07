2bb5d557c45c89924e068bb7e425c38a7a565f93
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-03-07 12:16:37 +0800

    Problem: awk syntax oops
    Solution: Separate arguments with commas

--- travis-test.sh
+++ travis-test.sh
@@ -8,12 +8,12 @@ make test | awk '
   }
   /^building \x27\/nix\/store\/.*[.]drv\x27/ {
     if(current_scope != "") {
-      printf "travis_fold:end:%s\r" current_scope
+      printf "travis_fold:end:%s\r", current_scope
     }
     current_scope=$0
     sub("building \x27/nix/store/", "", current_scope)
     sub("\x27.*", "", current_scope)
-    printf "travis_fold:start:%s\r" current_scope
+    printf "travis_fold:start:%s\r", current_scope
   }
   { print }
 '
