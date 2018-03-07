13e44ff4418235c38cd7767a327221ea164ea610
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-03-07 11:07:02 +0800

    Problem: Travis log is difficult to overview
    Solution: Add log folding, one fold per derivation

--- travis-test.sh
+++ travis-test.sh
@@ -1,2 +1,16 @@
 #! /usr/bin/env bash
-make test
+make test | awk '
+  BEGIN {
+    current_scope=""
+  }
+  /^building \x27\/nix\/store\/.*[.]drv\x27/ {
+    if(current_scope != "") {
+      print "travis_fold:end:" current_scope "\r"
+    }
+    current_scope=$0
+    sub("building \x27/nix/store/", "", current_scope)
+    sub("\x27.*", "", current_scope)
+    print "travis_fold:start:" current_scope "\r"
+  }
+  { print }
+'
