fractalide/racket2nix

commit a0cf4395f00b6518ee0674b26ccd9e0a2c7f1a11

Problem: Storing and updating default.nix is an extra step
Solution: Make the self-hosted generated default.nix our implied default.nix

We lose the check that we haven't modified racket2nix in a way that
affects default.nix, but most of the time modifying the output is
exactly what we want to achieve, and we do check if we are still
self-hosting without any issues.

Without an old default.nix to verify against, test-racket2nix becomes
redundant, so building racket-doc becomes our complete test.nix.

test.nix
@@ -10,24 +10,13 @@ in
 { pkgs ? import remotePkgs { }
 , stdenvNoCC ? pkgs.stdenvNoCC
 , racket ? pkgs.racket-minimal
-, default-nix ? pkgs.callPackage ./update-default.nix { inherit racket; }
+, racket2nix ? pkgs.callPackage ./. { inherit racket; }
 , colordiff ? pkgs.colordiff
-, racket-catalog ? default-nix.racket-catalog
+, racket-catalog ? racket2nix.racket-catalog
 }:
 
 let attrs = rec {
-  inherit default-nix;
-  test-racket2nix = stdenvNoCC.mkDerivation {
-    name = "test-racket2nix";
-    src = ./.;
-    phases = "unpackPhase buildPhase";
-    buildPhase = ''
-      echo output in $out
-      echo
-      diff -u default.nix ${default-nix} | tee $out | ${colordiff}/bin/colordiff
-    '';
-  };
-  racket2nix = (pkgs.callPackage default-nix { inherit racket; }).overrideDerivation (drv: rec { src = ./nix; srcs = [ src ]; });
+  inherit racket2nix;
   racket-doc-nix = stdenvNoCC.mkDerivation {
     name = "racket-doc.nix";
     buildInputs = [ racket ];
@@ -37,17 +26,6 @@ let attrs = rec {
     '';
   };
   racket-doc = pkgs.callPackage racket-doc-nix { inherit racket; };
-  test-racket2nix-all = stdenvNoCC.mkDerivation {
-    name = "test-racket2nix-all";
-    buildInputs = [ test-racket2nix racket-doc ];
-    phases = "installPhase";
-    installPhase = ''
-      tee $out <<EOF
-      ${test-racket2nix}
-      ${racket-doc}
-      EOF
-    '';
-  };
 };
 in
-attrs.test-racket2nix-all // attrs
+attrs.racket-doc // attrs

Makefile
@@ -1,21 +1,12 @@
-default: all
+default: racket2nix
 
-all: default.nix
-
-RACKET2NIX_FILES=update-default.nix nix/info.rkt nix/racket2nix.rkt
-
-default.nix.in.timestamp: $(RACKET2NIX_FILES)
-	nix-build update-default.nix --out-link default.nix.in
-	touch $@
-
-default.nix: default.nix.in.timestamp
-	cat < default.nix.in > $@.new
-	mv $@.new $@
+racket2nix:
+	nix-build --no-out-link
 
 pkgs-all:
-	nix-build update-default.nix --out-link pkgs-all -A racket-catalog
+	nix-build --out-link pkgs-all -A racket-catalog
 
 test:
 	nix-build test.nix --no-out-link 2>&1
 
-.PHONY: test
+.PHONY: racket2nix test

default.nix
@@ -1,161 +1,45 @@
-{ pkgs ? import <nixpkgs> {}
-, stdenv ? pkgs.stdenv
-, lib ? stdenv.lib
-, fetchurl ? pkgs.fetchurl
+{ pkgs ? import <nixpkgs> { }
+, stdenvNoCC ? pkgs.stdenvNoCC
 , racket ? pkgs.racket-minimal
-, racket-lib ? racket
-, unzip ? pkgs.unzip
-, racketIndexPatch ? builtins.toFile "racket-index.patch" ''
-    diff --git a/pkgs/racket-index/setup/scribble.rkt b/pkgs/racket-index/setup/scribble.rkt
-    index c79af9bf85..e4a1cf93e3 100644
-    --- a/pkgs/racket-index/setup/scribble.rkt
-    +++ b/pkgs/racket-index/setup/scribble.rkt
-    @@ -874,6 +874,7 @@
-             [(not latex-dest) (build-path (doc-dest-dir doc) file)]))
- 
-     (define (find-doc-db-path latex-dest user? main-doc-exists?)
-    +  (set! main-doc-exists? #t)
-       (cond
-        [latex-dest
-         (build-path latex-dest "docindex.sqlite")]
-  ''
+, cacert ? pkgs.cacert
 }:
 
-let mkRacketDerivation = lib.makeOverridable (attrs: stdenv.mkDerivation (rec {
-  circularBuildInputsStr = lib.concatStringsSep " " attrs.circularBuildInputs;
-  srcs = [ attrs.src ] ++ (map (input: input.src) attrs.reverseCircularBuildInputs);
-  phases = "unpackPhase patchPhase installPhase fixupPhase";
-  unpackPhase = ''
-    stripSuffix() {
-      stripped=$1
-      for suffix in .gz .tgz .zip .xz .tar; do
-        stripped=''${stripped%$suffix}
-      done
-      echo $stripped
-    }
-
-    runHook preUnpack
-    for unpackSrc in $srcs; do
-      unpackName=$(stripSuffix $(stripHash $unpackSrc))
-      mkdir $unpackName
-      cd $unpackName
-      unpackFile $unpackSrc
-      cd -
-      unpackedFiles=( $unpackName/* )
-      if [ "''${unpackedFiles[*]}" = "$unpackName/$unpackName" ]; then
-        mv $unpackName _
-        chmod u+w _/$unpackName
-        mv _/$unpackName $unpackName
-        rmdir _
-      fi
-    done
-    chmod u+w -R .
-    runHook postUnpack
-  '';
-
-  patchPhase = ''
-    case $name in
-      racket-index)
-        ( cd racket-index && patch -p3 < ${racketIndexPatch} )
-        ;;
-    esac
-  '';
-
-  racket-cmd = "${racket.out}/bin/racket -G $out/etc/racket -U -X $out/share/racket/collects";
-  raco = "${racket-cmd} -N raco -l- raco";
-  maxFileDescriptors = 2048;
-
-  passAsFile = [ "racketConfig" ];
-
-  installPhase = ''
-    if ! ulimit -n $maxFileDescriptors; then
-      echo >&2 If the number of allowed file descriptors is lower than '~2048,'
-      echo >&2 packages like drracket or racket-doc will not build correctly.
-      echo >&2 If raising the soft limit fails '(like it just did)', you will
-      echo >&2 have to raise the hard limit on your operating system.
-      echo >&2 Examples:
-      echo >&2 debian: https://unix.stackexchange.com/questions/127778
-      echo >&2 MacOS: https://superuser.com/questions/117102
-      exit 2
-    fi
-
-    mkdir -p $out/etc/racket $out/share/racket
-    sed -e 's|$out|'"$out|g" > $out/etc/racket/config.rktd < $racketConfigPath
-
-    remove_deps="${circularBuildInputsStr}"
-    if [[ -n $remove_deps ]]; then
-      sed -i $(printf -- '-e s/"%s"//g ' $remove_deps) $name/info.rkt
-    fi
-
-    echo ${racket-cmd}
-
-    mkdir -p $out/share/racket/collects/
-    for bootstrap_collection in racket compiler syntax setup openssl ffi file pkg planet; do
-      cp -rs ${racket.out}/share/racket/collects/$bootstrap_collection \
-        $out/share/racket/collects/
-    done
-    find $out/share/racket/collects -type d -exec chmod 755 {} +
-
-    # install and link us
-    if ${racket-cmd} -e "(require pkg/lib) (exit (if (member \"$name\" (installed-pkg-names #:scope (bytes->path (string->bytes/utf-8 \"${_racket-lib.out}/share/racket/pkgs\")))) 1 0))"; then
-      install_names=$(for install_info in ./*/info.rkt; do echo ''${install_info%/info.rkt}; done)
-      ${raco} pkg install --no-setup --copy --deps fail --fail-fast --scope installation $install_names
-      if [ -z "${circularBuildInputsStr}" ]; then
-        for install_name in $install_names; do
-          case ''${install_name#./} in
-            racket-doc|drracket) ;;
-            *)
-              ${raco} setup --no-user --no-pkg-deps --fail-fast --only --pkgs ''${install_name#./} |
-                sed -ne '/updating info-domain/,$p'
-              ;;
-          esac
-        done
-      fi
-    fi
-    find $out/share/racket/collects -lname '${_racket-lib.out}/share/racket/collects/*' -delete
-    find $out/share/racket/collects -type d -empty -delete
-  '';
-} // attrs));
-
-  _racket-lib = racket-lib;
-  _base = mkRacketDerivation rec {
-  name = "base";
-  src = fetchurl {
-    url = "https://download.racket-lang.org/releases/6.12/pkgs/base.zip";
-    sha1 = "842e2b328663a51a11b18ef6c0f971647227a231";
+let attrs = rec {
+  racket-catalog = stdenvNoCC.mkDerivation {
+    name = "pkgs-all";
+    src = ./.;
+    buildInputs = [ racket ];
+    phases = "unpackPhase installPhase";
+    installPhase = ''
+      $racket/bin/racket -N dump-catalogs ./nix/dump-catalogs.rkt https://download.racket-lang.org/releases/6.12/catalog/ > $out
+    '';
+    inherit racket;
+    SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+    outputHashMode = "flat";
+    outputHashAlgo = "sha256";
+    outputHash = "1p8f4yrnv9ny135a41brxh7k740aiz6m41l67bz8ap1rlq2x7pgm";
   };
-  buildInputs = [ unzip racket _racket-lib ];
-  circularBuildInputs = [  ];
-  reverseCircularBuildInputs = [  ];
-  racketConfig = ''
-#hash(
-  (share-dir . "$out/share/racket")
-  (links-search-files . ( "$out/share/racket/links.rktd" "${_racket-lib.out}/share/racket/links.rktd" ))
-  (pkgs-search-dirs . ( "$out/share/racket/pkgs" "${_racket-lib.out}/share/racket/pkgs" ))
-  (collects-search-dirs . ( "$out/share/racket/collects" "${_racket-lib.out}/share/racket/collects" ))
-  (doc-search-dirs . ( "$out/share/racket/doc" "${_racket-lib.out}/share/racket/doc" ))
-  (absolute-installation . #t)
-  (installation-name . ".")
-)
-  '';
+  racket2nix-stage0-nix = stdenvNoCC.mkDerivation {
+    name = "racket2nix-stage0.nix";
+    src = ./.;
+    buildInputs = [ racket ];
+    phases = "unpackPhase installPhase";
+    installPhase = ''
+      racket -N racket2nix ./nix/racket2nix.rkt --catalog ${racket-catalog} ./nix > $out
+    '';
   };
-  _nix = mkRacketDerivation rec {
-  name = "nix";
-  src = ./nix;
-  buildInputs = [ unzip racket _racket-lib _base ];
-  circularBuildInputs = [  ];
-  reverseCircularBuildInputs = [  ];
-  racketConfig = ''
-#hash(
-  (share-dir . "$out/share/racket")
-  (links-search-files . ( "$out/share/racket/links.rktd" "${_racket-lib.out}/share/racket/links.rktd" "${_base.out}/share/racket/links.rktd" ))
-  (pkgs-search-dirs . ( "$out/share/racket/pkgs" "${_racket-lib.out}/share/racket/pkgs" "${_base.out}/share/racket/pkgs" ))
-  (collects-search-dirs . ( "$out/share/racket/collects" "${_racket-lib.out}/share/racket/collects" "${_base.out}/share/racket/collects" ))
-  (doc-search-dirs . ( "$out/share/racket/doc" "${_racket-lib.out}/share/racket/doc" "${_base.out}/share/racket/doc" ))
-  (absolute-installation . #t)
-  (installation-name . ".")
-)
-  '';
+  racket2nix-stage0 = (pkgs.callPackage racket2nix-stage0-nix { inherit racket; }).overrideDerivation (drv: rec { src = ./nix; srcs = [ src ]; });
+  racket2nix-nix = stdenvNoCC.mkDerivation {
+    name = "racket2nix.nix";
+    src = ./.;
+    buildInputs = [ racket ];
+    phases = "unpackPhase installPhase";
+    installPhase = ''
+      racket -G ${racket2nix-stage0}/etc/racket -N racket2nix -l- nix/racket2nix --catalog ${racket-catalog} ./nix > $out
+      diff ${racket2nix-stage0-nix} $out
+    '';
   };
+  racket2nix = (pkgs.callPackage racket2nix-nix { inherit racket; }).overrideDerivation (drv: rec { src = ./nix; srcs = [ src ]; });
+};
 in
-_nix
+attrs.racket2nix // attrs

update-default.nix
@@ -1,45 +1,0 @@
-{ pkgs ? import <nixpkgs> { }
-, stdenvNoCC ? pkgs.stdenvNoCC
-, racket ? pkgs.racket-minimal
-, cacert ? pkgs.cacert
-}:
-
-let attrs = rec {
-  racket-catalog = stdenvNoCC.mkDerivation {
-    name = "pkgs-all";
-    src = ./.;
-    buildInputs = [ racket ];
-    phases = "unpackPhase installPhase";
-    installPhase = ''
-      $racket/bin/racket -N dump-catalogs ./nix/dump-catalogs.rkt https://download.racket-lang.org/releases/6.12/catalog/ > $out
-    '';
-    inherit racket;
-    SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
-    outputHashMode = "flat";
-    outputHashAlgo = "sha256";
-    outputHash = "1p8f4yrnv9ny135a41brxh7k740aiz6m41l67bz8ap1rlq2x7pgm";
-  };
-  default-nix-stage0 = stdenvNoCC.mkDerivation {
-    name = "racket2nix-stage0.nix";
-    src = ./.;
-    buildInputs = [ racket ];
-    phases = "unpackPhase installPhase";
-    installPhase = ''
-      racket -N racket2nix ./nix/racket2nix.rkt --catalog ${racket-catalog} ./nix > $out
-    '';
-  };
-  racket2nix-stage0 = (pkgs.callPackage default-nix-stage0 { inherit racket; }).overrideDerivation (drv: rec { src = ./nix; srcs = [ src ]; });
-  default-nix = stdenvNoCC.mkDerivation {
-    name = "racket2nix.nix";
-    src = ./.;
-    buildInputs = [ racket ];
-    phases = "unpackPhase installPhase";
-    installPhase = ''
-      racket -G ${racket2nix-stage0}/etc/racket -N racket2nix -l- nix/racket2nix --catalog ${racket-catalog} ./nix > $out
-      diff ${default-nix-stage0} $out
-    '';
-  };
-  racket2nix = (pkgs.callPackage default-nix { inherit racket; }).overrideDerivation (drv: rec { src = ./nix; srcs = [ src ]; });
-};
-in
-attrs.default-nix // attrs

commit 2f09ecd69f0b34246081942590bbe9778bc52e92

Problem: travis-test.sh not updated after renamings
This was introduced with a0cf439, but the problem fixed in f92d917
obscured it.

Solution: Update travis-test.sh

Now that `make` doesn't interfere with `make test`, move it higher, as
it fails faster.

test.nix
@@ -17,6 +17,7 @@ in
 
 let attrs = rec {
   inherit racket2nix;
+  inherit racket2nix-stage0;
   racket-doc-nix = stdenvNoCC.mkDerivation {
     name = "racket-doc.nix";
     buildInputs = [ racket2nix ];

travis-test.sh
@@ -2,9 +2,16 @@
 set -e
 set -u
 
-printf 'travis_fold:start:racket2nix prerequisites\r'
-nix-shell test.nix -A default-nix --run true
-printf 'travis_fold:end:racket2nix prerequisites\r'
+printf 'travis_fold:start:racket2nix-stage0.prerequisites\r'
+nix-shell test.nix -A racket2nix-stage0 --run true
+printf 'travis_fold:end:racket2nix-stage0.prerequisites\r'
+
+printf 'travis_fold:start:racket2nix\r'
+nix-build test.nix -A racket2nix
+printf 'travis_fold:end:racket2nix\r'
+
+make
+
 make test | awk '
   BEGIN {
     current_scope=""
@@ -20,8 +27,3 @@ make test | awk '
   }
   { print }
 '
-
-## `make` needs to come after `make test`, otherwise it fixes one
-## divergence `make test` is supposed to detect.
-
-make