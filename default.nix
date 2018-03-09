908db45c4af96c0bd48875fb46c50a56c9d55fbd
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-03-09 17:24:19 +0800

    Problem: cannot reach attributes in default.nix without building racket2nix-nix
    For nix to know whether racket2nix // attrs is allowed or not, it needs to
    build racket2nix-nix so it can evaluate racket2nix. This is confusing and
    stops e.g. pre-fetching from being exactly what it looks like.
    
    Solution: Split out stage0.nix and catalog.nix

--- default.nix
+++ default.nix
@@ -2,33 +2,12 @@
 , stdenvNoCC ? pkgs.stdenvNoCC
 , racket ? pkgs.racket-minimal
 , cacert ? pkgs.cacert
+, racket-catalog ? pkgs.callPackage ./catalog.nix { inherit racket; }
+, racket2nix-stage0 ? pkgs.callPackage ./stage0.nix { inherit racket; }
+, racket2nix-stage0-nix ? racket2nix-stage0.racket2nix-stage0-nix
 }:
 
 let attrs = rec {
-  racket-catalog = stdenvNoCC.mkDerivation {
-    name = "pkgs-all";
-    src = ./.;
-    buildInputs = [ racket ];
-    phases = "unpackPhase installPhase";
-    installPhase = ''
-      $racket/bin/racket -N dump-catalogs ./nix/dump-catalogs.rkt https://download.racket-lang.org/releases/6.12/catalog/ > $out
-    '';
-    inherit racket;
-    SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
-    outputHashMode = "flat";
-    outputHashAlgo = "sha256";
-    outputHash = "1p8f4yrnv9ny135a41brxh7k740aiz6m41l67bz8ap1rlq2x7pgm";
-  };
-  racket2nix-stage0-nix = stdenvNoCC.mkDerivation {
-    name = "racket2nix-stage0.nix";
-    src = ./.;
-    buildInputs = [ racket ];
-    phases = "unpackPhase installPhase";
-    installPhase = ''
-      racket -N racket2nix ./nix/racket2nix.rkt --catalog ${racket-catalog} ./nix > $out
-    '';
-  };
-  racket2nix-stage0 = (pkgs.callPackage racket2nix-stage0-nix { inherit racket; }).overrideDerivation (drv: rec { src = ./nix; srcs = [ src ]; });
   racket2nix-nix = stdenvNoCC.mkDerivation {
     name = "racket2nix.nix";
     src = ./.;
