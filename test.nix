63d23dfa2c97ae54955522b90b1a18f9da31b273
Stewart Mackenzie <setori88@gmail.com>
2018-03-31 02:06:40 +0800

    Problem: fetchgit fetches the entire history
    Solution: use fetchFromGithub which downloads just the
    tarball at that revision

--- test.nix
+++ test.nix
@@ -1,8 +1,8 @@
 let
   bootPkgs = import <nixpkgs> { };
-  bootFetchgit = bootPkgs.fetchgit;
-  remotePkgs = bootFetchgit {
-    url = "git://github.com/NixOS/nixpkgs-channels.git";
+  remotePkgs = bootPkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
     rev = "a66ce38acea505c4b3bfac9806669d2ad8b34efa";
     sha256 = "1jrz6lkhx64mvm0h4gky9b6iaazivq69smppkx33hmrm4553dx5h";
   };
