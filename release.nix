99e1c53a1fbb7381a9990cc9160aa63d360f5cde
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-05-30 16:00:42 +0800

    Problem: not prepared for hydra.fractalide.com
    Solution: Add release.nix

---
+++ release.nix
@@ -0,0 +1,13 @@
+let
+  racket2nixPath = relPath: "${builtins.toString <racket2nix>}/${relPath}";
+  nixpkgs = import (racket2nixPath "nixpkgs.nix") {};
+in
+{
+  pkgs-all = import (racket2nixPath "catalog.nix") {};
+  stage0-nix = (import (racket2nixPath "stage0.nix") {}).buildInputs;
+  racket2nix = import <racket2nix> {};
+  racket2nix-flat-nix = (import <racket2nix> {}).racket2nix-flat-nix;
+  test = import (racket2nixPath "test.nix") {};
+} // nixpkgs.lib.optionalAttrs nixpkgs.racket.meta.available {
+  racket2nix-full-racket = nixpkgs.callPackage <racket2nix> {};
+}
