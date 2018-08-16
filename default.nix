f7f921c34e4fbfabdfb68e0d5d10563af3d63496
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-08-16 10:52:03 +0800

    Problem: racket2nix-env is out of place
    Solution: Move it in under the racket2nixes as .buildEnv.

--- default.nix
+++ default.nix
@@ -6,23 +6,9 @@
 }:
 
 let
-  inherit (pkgs) buildEnv buildRacket nix nix-prefetch-git racket2nix-stage1;
-
-  # We put the deps both in paths and buildInputs, so you can use this either as just
-  # nix-shell -A racket2nix.buildEnv
-  # and get the environment-variable-only environment, or you can use it as
-  # nix-shell -p $(nix-build -A racket2nix.buildEnv)
-  # and get the symlink tree environment
-
-  racket2nix-env = buildEnv rec {
-    name = "racket2nix-env";
-    paths = [ nix nix-prefetch-git racket2nix-stage1 ];
-    buildInputs = paths;
-  };
-
+  inherit (pkgs) buildRacket racket2nix-stage1;
   attrs = pkgs // {
     racket2nix = racket2nix-stage1;
-    inherit racket2nix-env;
   };
 in
 if package == null then (attrs.racket2nix // attrs) else
--- stage1.nix
+++ stage1.nix
@@ -2,16 +2,28 @@
 }:
 
 let
-inherit (pkgs) buildRacketPackage nix racket2nix-stage0 runCommand;
+inherit (pkgs) buildEnv buildRacketPackage nix nix-prefetch-git racket2nix-stage0 runCommand;
 
 # Don't just build a flat package, build it with flat racket2nix.
 buildRacketPackageFlat = package: (pkgs.overridePkgs (oldAttrs: {
   overlays = oldAttrs.overlays ++ [ (self: super: { racket2nix = super.racket2nix.flat; }) ];
 })).buildRacket { inherit package; flat = true; };
 
-testAndVerify = drv: drv.overrideAttrs (oldAttrs: {
+addAttrs = drv: drv.overrideAttrs (oldAttrs: {
   buildInputs = oldAttrs.buildInputs ++ [ nix verify ];
   postInstall = "$out/bin/racket2nix --test";
+
+  # We put the deps both in paths and buildInputs, so you can use this either as just
+  # nix-shell -A racket2nix.buildEnv
+  # and get the environment-variable-only environment, or you can use it as
+  # nix-shell -p $(nix-build -A racket2nix.buildEnv)
+  # and get the symlink tree environment
+
+  buildEnv = buildEnv rec {
+    name = "${drv.name}-env";
+    paths = [ nix nix-prefetch-git drv ];
+    buildInputs = paths;
+  };
 });
 
 stage1 = (buildRacketPackage ./nix) // {
@@ -24,11 +36,11 @@ verify = runCommand "verify-stage1.sh" {} ''
   diff ${racket2nix-stage0.flat.nix} ${stage1.flat.nix} >> $out
 '';
 
-racket2nix-stage1 = (testAndVerify stage1).overrideAttrs (oldAttrs: {
+racket2nix-stage1 = (addAttrs stage1).overrideAttrs (oldAttrs: {
   name = "racket2nix-stage1";
 }) // {
   inherit (stage1) nix;
-  flat = (testAndVerify stage1.flat).overrideAttrs (oldAttrs: {
+  flat = (addAttrs stage1.flat).overrideAttrs (oldAttrs: {
     name = "racket2nix-stage1.flat";
   }) // { inherit (stage1.flat) nix; };
 };
