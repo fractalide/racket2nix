e5ec81762c3e1cb8d9cc90fb1aad194482d9bfc5
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-08-16 09:55:43 +0800

    Problem: Too much cruft in default.nix
    Solution: Remove everything that is no longer used.

--- default.nix
+++ default.nix
@@ -6,24 +6,16 @@
 }:
 
 let
-inherit (pkgs) buildEnv buildRacket nix nix-prefetch-git racket racket2nix-stage0 stdenvNoCC;
-attrs = rec {
-  inherit buildRacket;
-  racket2nix-stage1 = pkgs.callPackage ./stage1.nix {};
-  racket2nix-stage1-nix = racket2nix-stage1.nix;
-  racket2nix = racket2nix-stage1
-
-  racket2nix-flat-stage0 = racket2nix-stage0.flat;
-  racket2nix-flat-stage0-nix = racket2nix-stage0.flat.nix;
-  racket2nix-flat-stage1-nix = racket2nix-stage1.flat.nix;
-  racket2nix-flat-nix = racket2nix-flat-stage1-nix;
-
+  inherit (pkgs) buildRacket nix nix-prefetch-git racket2nix-stage1 stdenvNoCC;
   racket2nix-env = stdenvNoCC.mkDerivation {
     phases = [];
-    buildInputs = [ nix nix-prefetch-git racket2nix ];
+    buildInputs = [ nix nix-prefetch-git racket2nix-stage1 ];
     name = "racket2nix-env";
   };
-};
+  attrs = pkgs // {
+    racket2nix = racket2nix-stage1;
+    inherit racket2nix-env;
+  };
 in
 if package == null then (attrs.racket2nix // attrs) else
 buildRacket { inherit catalog package flat; }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -11,6 +11,7 @@ pkgsFn = args: args.pkgs ((removeAttrs args [ "pkgs" ]) // { overlays = [ (self:
   racket = self.racket-minimal;
 
   racket2nix-stage0 = self.callPackage ../stage0.nix {};
+  racket2nix-stage1 = self.callPackage ../stage1.nix {};
   racket2nix = self.racket2nix-stage0;
   inherit (self.callPackage ../build-racket.nix {}) buildRacket buildRacketPackage;
 }; in racket2nix-pkgs // { inherit racket2nix-pkgs; }) ] ++ args.overlays; });
