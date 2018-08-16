adb8471f1c306c099481f891a51227d08d092a7f
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-08-16 09:35:34 +0800

    Problem: stage0: mkDerivation boilerplate
    runCommand is more readable.
    
    Solution: Use runCommand.

--- stage0.nix
+++ stage0.nix
@@ -3,16 +3,13 @@
 }:
 
 let
-  inherit (pkgs) nix racket stdenvNoCC;
-  stage0-nix = stdenvNoCC.mkDerivation {
-    name = "racket2nix-stage0.nix";
+  inherit (pkgs) nix racket runCommand;
+  stage0-nix = runCommand "racket2nix-stage0.nix" {
     src = ./nix;
     buildInputs = [ nix racket ];
-    phases = "unpackPhase installPhase";
-    installPhase = ''
-      racket -N racket2nix ./racket2nix.rkt --catalog ${catalog} $src > $out
-    '';
-  };
+  } ''
+    racket -N racket2nix $src/racket2nix.rkt --catalog ${catalog} $src > $out
+  '';
   stage0 = pkgs.callPackage stage0-nix { inherit racket; };
 in
 stage0 // { nix = stage0-nix; }
