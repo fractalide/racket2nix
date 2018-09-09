b6a002b09e753f3cb0924029af255e5cd13ef082
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-09-10 01:59:09 +0800

    Problem: catalog.rktd is out of date
    We have updated racket and the pins on the upstream catalogs.
    
    Solution: Generate catalog.rktd.

--- update-catalog.sh
+++ update-catalog.sh
@@ -7,5 +7,9 @@ set -u
 SCRIPT_NAME=${BASH_SOURCE[0]##*/}
 cd "${BASH_SOURCE[0]%${SCRIPT_NAME}}"
 
+echo NOTE: nix-prefetch-git will use your ${XDG_RUNTIME_DIR:-${TMP:-/tmp}}
+echo NOTE: to download some pretty large git repos. Make sure you have
+echo NOTE: hundreds of megabytes of space.
+
 ./racket2nix --catalog $(nix-build catalog.nix) --export-catalog > catalog.rktd.new
 mv catalog.rktd.new catalog.rktd
