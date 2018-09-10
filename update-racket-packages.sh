ae139a345364674707463d5e49a3bca5907d86d6
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-09-10 15:44:43 +0800

    Problem: Didn't actually add update-racket-packages.sh
    Solution: Do it.

---
+++ update-racket-packages.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env nix-shell
+#! nix-shell --pure -p bash nix racket-minimal -i bash
+
+set -e
+set -u
+
+SCRIPT_NAME=${BASH_SOURCE[0]##*/}
+cd "${BASH_SOURCE[0]%${SCRIPT_NAME}}"
+
+./racket2nix --catalog catalog.rktd > racket-packages.nix.new
+mv racket-packages.nix{.new,}
