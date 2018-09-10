e98e3c7fcfa491d4ef5e5fb08eb9fb952a8d1bf5
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-09-10 16:53:39 +0800

    Problem: gui-lib: Implicit dependency on icons
    racket-doc fails to build:
    
        $ nix-build --argstr package racket-doc
    
        include-bitmap: could not load #<path:/nix/store/kd1l6lz8h7dns7j5snkqza9jv9r88y6p-racket-minimal-7.0-gui-lib-env/share/racket/pkgs/gui-lib/icons/break.png>: with-input-from-file: cannot open input file
          path: /nix/store/kd1l6lz8h7dns7j5snkqza9jv9r88y6p-racket-minimal-7.0-gui-lib-env/share/racket/pkgs/gui-lib/icons/break.png
          system error: No such file or directory; errno=2
    
    Solution: Add a dependency override in gui-lib and its dependees.
    
    Reported upstream as racket/gui#114

--- build-racket-default-overlay.nix
+++ build-racket-default-overlay.nix
@@ -1,5 +1,15 @@
 let inherit (import ./pkgs {}) lib; in
 
 self: super:
-{
+lib.optionalAttrs (super ? "deinprogramm-signature" && super ? "icons") {
+  deinprogramm-signature = super.deinprogramm-signature.overrideRacketDerivation (oldAttrs: { racketBuildInputs = oldAttrs.racketBuildInputs ++ [ self.icons ]; });
+} //
+lib.optionalAttrs (super ? "deinprogramm-signature+htdp-lib" && super ? "icons") {
+  "deinprogramm-signature+htdp-lib" = super."deinprogramm-signature+htdp-lib".overrideRacketDerivation (oldAttrs: { racketBuildInputs = oldAttrs.racketBuildInputs ++ [ self.icons ]; });
+} //
+lib.optionalAttrs (super ? "gui-lib" && super ? "icons") {
+  gui-lib = super.gui-lib.overrideRacketDerivation (oldAttrs: { racketBuildInputs = oldAttrs.racketBuildInputs ++ [ self.icons ]; });
+} //
+lib.optionalAttrs (super ? "htdp-lib" && super ? "icons") {
+  htdp-lib = super.htdp-lib.overrideRacketDerivation (oldAttrs: { racketBuildInputs = oldAttrs.racketBuildInputs ++ [ self.icons ]; });
 }
