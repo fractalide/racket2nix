8f155f505efaf83f3ff493095e0953de7236de5f
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2018-09-13 01:08:27 +0800

    Problem: release.nix: No check for racket-packages.nix
    Solution: Add check that it's up-to-date with the generator.

--- release.nix
+++ release.nix
@@ -22,6 +22,19 @@ let
 in
   (genJobs (pkgs {})) //
   {
+    racket-packages-updated = (pkgs {}).runCommand "racket-packages-updated" rec {
+      src = <racket2nix>;
+      inherit (pkgs {}) racket2nix;
+      buildInputs = [ racket2nix ];
+    } ''
+      set -e; set -u
+      racket2nix --catalog $src/catalog.rktd > racket-packages.nix
+      if ! diff -u $src/racket-packages.nix racket-packages.nix > $out; then
+        echo racket-packages.nix has not been kept up-to-date, please regenerate and commit.
+        echo missing changes:
+        diff -u racket-packages.nix $src/racket-packages.nix
+      fi
+    '';
     latest-nixpkgs = genJobs (pkgs { pkgs = import <nixpkgs>; });
     x86_64-darwin = genJobs (pkgs { system = "x86_64-darwin"; }) // {
       latest-nixpkgs = genJobs (pkgs { pkgs = import <nixpkgs>; system = "x86_64-darwin"; });
@@ -31,5 +44,6 @@ in
   } // lib.optionalAttrs isTravis {
     stage0-nix-prerequisites = (pkgs {}).racket2nix-stage0.buildInputs;
     travisOrder = [ "pkgs-all" "stage0-nix-prerequisites" "racket2nix" "tests.light-tests"
+                    "racket-packages-updated"
                     "racket-full.racket2nix" "api.override-racket-derivation" ];
   }
