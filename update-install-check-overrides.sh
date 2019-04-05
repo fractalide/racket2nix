bb569c1a2e7e6b3b05efd91ffb5a0bc9d36d5eaf
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2019-04-05 14:45:50 +0800

    Problem: update-*-overrides: May go silent for a long time
    Solution: Provide more progress feedback while dependencies building.

--- update-install-check-overrides.sh
+++ update-install-check-overrides.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell --quiet -i bash -p bash
+#! nix-shell --quiet -i bash -p bash coreutils gawk gnused
 
 set -euo pipefail
 
@@ -10,7 +10,12 @@ packages=($(nix-instantiate --eval -E  'builtins.concatStringsSep " " (builtins.
 i=1
 for package in ${packages[@]}; do
   echo >&2 $((i++))/${#packages[@]} $package
-  nix-build --no-build-output --quiet -E '{package}:
+  nix-build 2> >(sed -ue 's,/nix/store/.\{33\},,' | stdbuf -o0 gawk '
+    BEGIN { start = systime() }
+    { printf "\r%s %s\033[K", strftime("%H:%M:%S", systime() - start, 1), substr($0, 1, 68) }
+    END { printf "\r\033[K" }
+  ' >&2) \
+    --no-build-output -E '{package}:
     (import ./. { inherit package; }).overrideRacketDerivation (oldAttrs: { doInstallCheck = true; })' \
   --argstr package $package | xargs nix-store --read-log | sed -ne '/running install tests/,$p' |
   grep -A 1 'tests passed' >&2 &&
