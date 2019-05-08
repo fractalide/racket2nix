b03de01edc3a9bcaa1e3ec524007f090808f1e40
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2019-05-09 03:41:22 +0800

    Problem: update-catalog: May fail with blank catalog.rktd
    If catalog.nix fails to build, update-catalog goes ahead and writes a
    blank catalog.rktd.new, then moves it to catalog.rktd.
    
    Solution: Make running nix-build its own line, so the script can fail faster.

--- update-catalog.sh
+++ update-catalog.sh
@@ -11,6 +11,7 @@ echo NOTE: nix-prefetch-git will use your ${XDG_RUNTIME_DIR:-${TMP:-/tmp}}
 echo NOTE: to download some pretty large git repos. Make sure you have
 echo NOTE: hundreds of megabytes of space.
 
-./racket2nix --catalog $(nix-build catalog.nix) --export-catalog |
+catalog=$(nix-build --no-out-link catalog.nix)
+./racket2nix --catalog $catalog --export-catalog |
   racket -e '(pretty-write (read))' > catalog.rktd.new
 mv catalog.rktd.new catalog.rktd
