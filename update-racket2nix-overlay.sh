46fb7f84469c887939aabcaaf156b022e8feb3a2
Claes Wallin (韋嘉誠) <claes.wallin@greatsinodevelopment.com>
2019-05-15 23:35:25 +0800

    Problem: racket2nix-overlay-updated: Cannot succeed on NixOS
    In the sandbox there is no /usr/bin/env, so running ./racket2nix
    inside a derivation fails.
    
    Solution: update-racket2nix-overlay: Call ./racket2nix with explicit bash.
    
    ./release.sh now passes on NixOS.

--- update-racket2nix-overlay.sh
+++ update-racket2nix-overlay.sh
@@ -8,7 +8,7 @@ cd "${BASH_SOURCE[0]%${SCRIPT_NAME}}"
 
 OUTPUT_FILE=build-racket-racket2nix-overlay.nix
 
-./racket2nix --thin ./nix | 
+bash ./racket2nix --thin ./nix | 
   sed -e 's,src =.*,src = ./nix;,p' -e '/src =/,/}/d' \
   > $OUTPUT_FILE.new
 mv $OUTPUT_FILE{.new,}
